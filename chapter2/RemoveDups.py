173271d6e21a0befbebcf8f4b7829c34fcf446ae
Shashank Sunkavalli <ssunkav@searshc.com>
2018-05-28 15:36:55 -0500

    followup solution

--- chapter2/RemoveDups.py
+++ chapter2/RemoveDups.py
@@ -1,14 +1,13 @@
 from LinkedList import LinkedList
 
 def remove_dups(linked_list):
-
     if len(linked_list) < 2:
         return linked_list
     else:
         p = linked_list.head
         elems = set(str(p))
         
-        while p is not None and p.next is not None:
+        while p.next is not None:
             if str(p.next) in elems:
                 p.next = p.next.next
             else:
@@ -16,8 +15,30 @@ def remove_dups(linked_list):
                 p = p.next
         return linked_list
 
+def remove_dups_followup(linked_list):
+    if len(linked_list) < 2:
+        return linked_list
+    else:
+        cur = linked_list.head
+        while cur is not None:
+            p = cur
+            while p.next is not None:
+                if str(p.next) == str(cur):
+                    p.next = p.next.next
+                else:
+                    p = p.next
+            cur = cur.next
+        return linked_list
+
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.generate(20,1,10)
+    ll.generate(50,1,10)
     print(ll)
-    print(remove_dups(ll))
+    remove_dups(ll)
+    print(ll,len(ll))
+
+    ll2 = LinkedList()
+    ll2.generate(50,1,10)
+    print(ll2)
+    remove_dups_followup(ll2)
+    print(ll2,len(ll2))
